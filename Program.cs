c91b9b73e27f642d6183c978c80a9b73daf3b990
HenryKasai <henrykasai1775@gmail.com>
2020-02-28 21:33:08 -0300

    Solução de exercício

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Arte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("     z$$$$$. $$");
+            Console.WriteLine("    $$$$$$$$$$$");
+            Console.WriteLine("   $$$$$$**$$$$             eeeeer");
+            Console.WriteLine("  $$$$$%   '$$$             $$$$$F");
+            Console.WriteLine(" 4$$$$P     *$$             *$$$$F");
+            Console.WriteLine(" $$$$$      '$$    .ee.      ^$$$F            ..e.");
+            Console.WriteLine(" $$$$$           .$$$$$$b     $$$F 4$$$$$$   $$$$$$c");
+            Console.WriteLine("4$$$$F          4$$$  $$$$    $$$F '*$$$$*  $$$P $$$$");
+            Console.WriteLine("4$$$$F         .$$$F  ^$$$b   $$$F  J$$$   $$$$  ^$$$.");
+            Console.WriteLine("4$$$$F         d$$$    $$$$   $$$F J$$P   .$$$F   $$$$");
+            Console.WriteLine("4$$$$F         $$$$    3$$$   $$$FJ$$P    4$$$    $$$$");
+            Console.WriteLine("4$$$$F        4$$$$    4$$$   $$$$$$$r    $$$$$$$$$$$$");
+            Console.WriteLine("4$$$$$        4$$$$    4$$$   $$$$$$$$    $$$$********");
+            Console.WriteLine(" $$$$$        4$$$$    4$$$   $$$F4$$$b   *$$$r");
+            Console.WriteLine(" 3$$$$F       d$$$$    $$$$   $$$F *$$$F  4$$$L     ");
+            Console.WriteLine("  $$$$$.     d$$$$$    $$$$   $$$F  $$$$.  $$$$    z$P");
+            Console.WriteLine("  $$$$$e..d$$$ $$$b    4$$$   $$$L   $$$$  $$$b..d$$");
+            Console.WriteLine("   *$$$$$$$$$  ^$$$be$$$$     $$$$$  $$$$$  3$$$$$");
+            Console.WriteLine("   ^*$$$$P        *$$$$*      $$$$$$$   $$$$");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ReadKey();
+          
+
+
+        }
+    }
+}
